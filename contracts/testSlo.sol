45fb8cdaadb5112209d0d43a2abdb5c1960d7bf6
AndyLuffy <huangshiyang16@yeah.net>
2025-09-26 16:02:18 +0800

    main

---
+++ contracts/testSlo.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract teatSlo{
+
+    function sayHello(string memory name) public view  returns(string memory) {
+        return name;
+    }
+
+}
